b6cacdafaec52d9ee5d984ebf71712ad841c8af6
zdoh <denfairycat@gmail.com>
2017-08-08 19:45:09 +0300

    v0.7 small changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,6 @@ import model.*;
 import service.*;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
