9440ed24b012991591ebb400c84f7b08ba1f7880
zdoh <denfairycat@gmail.com>
2017-08-10 17:02:02 +0300

    v0.10 rename Main to SimpleTestForLogic

--- src/main/java/SimpleTestForLogic.java
+++ src/main/java/SimpleTestForLogic.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class Main {
+public class SimpleTestForLogic {
     public static void main(String[] args) {
         PersonService personService = new PersonServiceImpl();
         SiteService siteService = new SiteServiceImpl();
