70a35bca518e329511fef214e42a5db4a39f36c4
goryainov_vn <goryainov.vn@gmail.com>
2017-08-12 18:15:03 +0300

    Added fake data display in the main program windows in TableView. (google translate)

--- src/app/model/User.java
+++ src/app/model/User.java
@@ -1,29 +1,34 @@
 package app.model;
 
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public class User {
     private Integer id;
-    private String name;
-    private String password;
-    private String email;
-    private boolean administrator;
+    private StringProperty name;
+    private StringProperty password;
+    private StringProperty email;
+    private BooleanProperty administrator;
 
     public User() {
     }
 
     public User(String name, String password, String email, boolean administrator) {
-        this.name = name;
-        this.password = password;
-        this.email = email;
-        this.administrator = administrator;
+        this.name = new SimpleStringProperty(name);
+        this.password = new SimpleStringProperty(password);
+        this.email = new SimpleStringProperty(email);
+        this.administrator = new SimpleBooleanProperty(administrator);
     }
 
 
     public User(Integer id, String name, String password, String email, boolean administrator) {
         this.id = id;
-        this.name = name;
-        this.password = password;
-        this.email = email;
-        this.administrator = administrator;
+        this.name = new SimpleStringProperty(name);
+        this.password = new SimpleStringProperty(password);
+        this.email = new SimpleStringProperty(email);
+        this.administrator = new SimpleBooleanProperty(administrator);
     }
 
 
@@ -36,35 +41,51 @@ public class User {
     }
 
     public String getName() {
-        return name;
+        return name.get();
     }
 
     public void setName(String name) {
-        this.name = name;
+        this.name.set(name);
+    }
+
+    public StringProperty nameProperty() {
+        return name;
     }
 
     public String getEmail() {
-        return email;
+        return email.get();
     }
 
     public void setEmail(String email) {
-        this.email = email;
+        this.email.set(email);
+    }
+
+    public StringProperty emailProperty() {
+        return email;
     }
 
     public boolean isAdministrator() {
-        return administrator;
+        return administrator.get();
     }
 
     public void setAdministrator(boolean administrator) {
-        this.administrator = administrator;
+        this.administrator.set(administrator);
+    }
+
+    public BooleanProperty administratorProperty() {
+        return administrator;
     }
 
     public String getPassword() {
-        return password;
+        return password.get();
     }
 
     public void setPassword(String password) {
-        this.password = password;
+        this.password.set(password);
+    }
+
+    public StringProperty passwordProperty() {
+        return password;
     }
 
     public boolean isNew() {
--- src/app/view/administration/users/UsersManageController.java
+++ src/app/view/administration/users/UsersManageController.java
@@ -1,10 +1,37 @@
 package app.view.administration.users;
 
+import app.model.User;
+import app.service.UserService;
+import app.service.UserServiceImpl;
 import app.view.ModalWindows;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.input.MouseEvent;
 
-public class UsersManageController implements ModalWindows {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class UsersManageController implements ModalWindows, Initializable {
+
+    private UserService userService = new UserServiceImpl();
+
+    @FXML
+    private TableView<User> usersTableList;
+
+    @FXML
+    private TableColumn<User, String> userName;
+
+    @FXML
+    private TableColumn<User, Boolean> userRole;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        userName.setCellValueFactory(userData -> userData.getValue().nameProperty());
+        userRole.setCellValueFactory(roleData -> roleData.getValue().administratorProperty());
+        usersTableList.setItems(userService.getAll());
+    }
 
     @FXML
     public void addEditUser(MouseEvent mouseEvent) {
