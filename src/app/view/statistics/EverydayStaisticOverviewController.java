60a93ead5aa209dc4f0ce3a523c95fd340deae56
goryainov_vn <goryainov.vn@gmail.com>
2017-08-16 22:43:26 +0300

    Not a successful attempt to display data in the windows of general and daily statistics. Fixed display of the general statistics window.

--- src/app/view/statistics/EverydayStaisticOverviewController.java
+++ src/app/view/statistics/EverydayStaisticOverviewController.java
@@ -1,4 +1,20 @@
 package app.view.statistics;
 
-public class EverydayStaisticOverviewController {
+import app.model.Site;
+import app.view.confirmation.ManageConfirmation;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+
+public class EverydayStaisticOverviewController implements ManageConfirmation {
+
+    @FXML
+    public Button cancelButton;
+    @FXML
+    private ChoiceBox<Site> siteListChoiceBox;
+
+    public void closeModalWindow(ActionEvent actionEvent) {
+        onExitButtonFromWindow(cancelButton);
+    }
 }
